eed737c30374d1a1209cf8226779e14a2aa30f9b
Randy <raoulrandy3@gmail.com>
2026-04-22 09:54:40 +0300

    feat: member endpoint

--- src/main/java/school/hei/exam/agriculturalfederation/repository/MemberRepository.java
+++ src/main/java/school/hei/exam/agriculturalfederation/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package school.hei.exam.agriculturalfederation.repository;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDate;
@@ -25,19 +26,20 @@ public class MemberRepository {
         try {
             List<Member> members = new ArrayList<>();
             PreparedStatement preparedStatement = connection.prepareStatement(
-                ""
+                "SELECT id, last_name, first_name, birth_date, gender, address, "+
+                "job, phone, email, join_date, poste FROM membre"
             );
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 Member member = new Member();
-                member.setId(resultSet.getString("member_id"));
-                member.setFirstName(resultSet.getString("member_first_name"));
-                member.setLastName(resultSet.getString("member_last_name"));
-                member.setAddress(resultSet.getString("member_address"));
-                member.setBirthday(LocalDate.parse(resultSet.getString("member_birthday")));
-                member.setEmail(resultSet.getString(""));
-                member.setOccupation(OccupationEnum.valueOf(resultSet.getString("")));
-                member.setPhoneNumber(resultSet.getString(""));
+                member.setId(resultSet.getString("id"));
+                member.setFirstName(resultSet.getString("first_name"));
+                member.setLastName(resultSet.getString("last_name"));
+                member.setAddress(resultSet.getString("address"));
+                member.setBirthday(LocalDate.parse(resultSet.getString("birth_date")));
+                member.setEmail(resultSet.getString("email"));
+                member.setOccupation(OccupationEnum.JUNIOR);
+                member.setPhoneNumber(resultSet.getString("phone"));
             }
             return members;
         } catch (Exception e) {
@@ -47,16 +49,65 @@ public class MemberRepository {
     public List<Member> createMembers (List<MemberInscription> membersInscription){
         try {
             List<String> listOfIds = new ArrayList<>();
+            List<Name> name = new ArrayList<>();
             PreparedStatement preparedStatement = connection.prepareStatement(
-                ""
+                "INSERT INTO member (first_name, last_name, birth_date, gender, address, "+
+                "phone, email) " +
+                "VALUES (?, ?, ?, ?, ?, ?)"
             );
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                listOfIds.add(resultSet.getString("id"));
+            for (MemberInscription memberInscription : membersInscription) {
+                name.add(new Name(memberInscription.getFirstName(),memberInscription.getLastName()));
+                preparedStatement.setString(1, memberInscription.getFirstName());
+                preparedStatement.setString(2, memberInscription.getLastName());
+                preparedStatement.setDate(3, Date.valueOf(memberInscription.getBirthday()));
+                preparedStatement.setObject(4, memberInscription.getGender());
+                preparedStatement.setString(5, memberInscription.getPhoneNumber());
+                preparedStatement.setString(6, memberInscription.getEmail());
+                preparedStatement.addBatch();
             }
+            preparedStatement.executeBatch();
+            listOfIds = getIdByName(name);
             return findMembersById(listOfIds);
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private class Name {
+        private String firstName;
+        private String lastName;
+        
+        public Name(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public String getFirstName() {
+            return firstName;
+        }
+        public String getLastName() {
+            return lastName;
+        }
+        
+        
+    }
+    public List<Integer> getIdByName (List<Name> name){
+        try {
+            List<Integer> listOfids = new ArrayList<>();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT id FROM member WHERE first_name = ? AND last_name = ? "
+            );
+            for (Name nameToGet : name) {
+                preparedStatement.setString(1, nameToGet.getFirstName());
+                preparedStatement.setString(2,nameToGet.getLastName());
+            }
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                listOfids.add(resultSet.getInt("id"));
+            }
+
+            return listOfids;
+        } catch (Exception e) {
+            throw new RuntimeException();
         }
     }
     
