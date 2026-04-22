a32f4ff343fed9c3b483f474eabcdde39aebbae9
harena-mihaja <mihaja2506@gmail.com>
2026-04-22 16:49:58 +0300

    feat: add inscription related file

---
+++ src/main/java/school/hei/exam/agriculturalfederation/dto/CreateMemberDTO.java
@@ -0,0 +1,193 @@
+package school.hei.exam.agriculturalfederation.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class CreateMemberDTO {
+    private MemberInformationDTO information;
+    private String collectivityIdentifier;
+    private List<RefereeDTO> referees;
+    private Boolean registrationFeePaid;
+    private Boolean membershipDuesPaid;
+
+    public CreateMemberDTO() {
+    }
+
+    public CreateMemberDTO(MemberInformationDTO information, String collectivityIdentifier,
+                           List<RefereeDTO> referees, Boolean registrationFeePaid, Boolean membershipDuesPaid) {
+        this.information = information;
+        this.collectivityIdentifier = collectivityIdentifier;
+        this.referees = referees;
+        this.registrationFeePaid = registrationFeePaid;
+        this.membershipDuesPaid = membershipDuesPaid;
+    }
+
+    public MemberInformationDTO getInformation() {
+        return information;
+    }
+
+    public void setInformation(MemberInformationDTO information) {
+        this.information = information;
+    }
+
+    public String getCollectivityIdentifier() {
+        return collectivityIdentifier;
+    }
+
+    public void setCollectivityIdentifier(String collectivityIdentifier) {
+        this.collectivityIdentifier = collectivityIdentifier;
+    }
+
+    public List<RefereeDTO> getReferees() {
+        return referees;
+    }
+
+    public void setReferees(List<RefereeDTO> referees) {
+        this.referees = referees;
+    }
+
+    public Boolean getRegistrationFeePaid() {
+        return registrationFeePaid;
+    }
+
+    public void setRegistrationFeePaid(Boolean registrationFeePaid) {
+        this.registrationFeePaid = registrationFeePaid;
+    }
+
+    public Boolean getMembershipDuesPaid() {
+        return membershipDuesPaid;
+    }
+
+    public void setMembershipDuesPaid(Boolean membershipDuesPaid) {
+        this.membershipDuesPaid = membershipDuesPaid;
+    }
+
+    public static class MemberInformationDTO {
+        private String firstName;
+        private String lastName;
+        private LocalDate birthDate;
+        private String gender;
+        private String address;
+        private String profession;
+        private String phoneNumber;
+        private String email;
+        private String occupation;
+
+        public MemberInformationDTO() {
+        }
+
+        public MemberInformationDTO(String firstName, String lastName, LocalDate birthDate, String gender,
+                                    String address, String profession, String phoneNumber, String email, String occupation) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.gender = gender;
+            this.address = address;
+            this.profession = profession;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.occupation = occupation;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public LocalDate getBirthDate() {
+            return birthDate;
+        }
+
+        public void setBirthDate(LocalDate birthDate) {
+            this.birthDate = birthDate;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getProfession() {
+            return profession;
+        }
+
+        public void setProfession(String profession) {
+            this.profession = profession;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getOccupation() {
+            return occupation;
+        }
+
+        public void setOccupation(String occupation) {
+            this.occupation = occupation;
+        }
+    }
+
+    public static class RefereeDTO {
+        private String memberIdentifier;
+        private String relationshipNature;
+
+        public RefereeDTO() {
+        }
+
+        public RefereeDTO(String memberIdentifier, String relationshipNature) {
+            this.memberIdentifier = memberIdentifier;
+            this.relationshipNature = relationshipNature;
+        }
+
+        public String getMemberIdentifier() {
+            return memberIdentifier;
+        }
+
+        public void setMemberIdentifier(String memberIdentifier) {
+            this.memberIdentifier = memberIdentifier;
+        }
+
+        public String getRelationshipNature() {
+            return relationshipNature;
+        }
+
+        public void setRelationshipNature(String relationshipNature) {
+            this.relationshipNature = relationshipNature;
+        }
+    }
+}
---
+++ src/main/java/school/hei/exam/agriculturalfederation/dto/MemberInscription.java
@@ -0,0 +1,65 @@
+package school.hei.exam.agriculturalfederation.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class MemberInscription {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String gender;
+    private String address;
+    private String profession;
+    private String phoneNumber;
+    private String email;
+    private String occupation;
+    private List<String> refereeIds;
+    private Boolean registrationFeePaid;
+    private Boolean membershipDuesPaid;
+
+    public MemberInscription() {}
+
+    public MemberInscription(String firstName, String lastName, LocalDate birthDate, String gender,
+            String address, String profession, String phoneNumber, String email,
+            List<String> refereeIds, Boolean registrationFeePaid, Boolean membershipDuesPaid) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.address = address;
+        this.profession = profession;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.refereeIds = refereeIds;
+        this.registrationFeePaid = registrationFeePaid;
+        this.membershipDuesPaid = membershipDuesPaid;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public LocalDate getBirthDate() { return birthDate; }
+    public void setBirthDate(LocalDate birthDate) { this.birthDate = birthDate; }
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+    public String getProfession() { return profession; }
+    public void setProfession(String profession) { this.profession = profession; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getOccupation() { return occupation; }
+    public void setOccupation(String occupation) { this.occupation = occupation; }
+    public List<String> getRefereeIds() { return refereeIds; }
+    public void setRefereeIds(List<String> refereeIds) { this.refereeIds = refereeIds; }
+    public Boolean getRegistrationFeePaid() { return registrationFeePaid; }
+    public void setRegistrationFeePaid(Boolean registrationFeePaid) { this.registrationFeePaid = registrationFeePaid; }
+    public Boolean getMembershipDuesPaid() { return membershipDuesPaid; }
+    public void setMembershipDuesPaid(Boolean membershipDuesPaid) { this.membershipDuesPaid = membershipDuesPaid; }
+}
